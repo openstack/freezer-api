ef24c928e037ef66b37b1520a5db922f96d7cbfc
Cai Hui <cai.hui@zte.com.cn>
2018-10-06 23:44:30 -0400

    Add freezer_api.api.v2.clients unit test cases
    1. Add 'freezer_api.api.v2.clients'  module's unit test cases.
    2. Add "project_id" key-value to 'fake_client_info_0'&'fake_client_info_1' dicts.
    
    Change-Id: I61ed4dab13ff17b4612f3454b5b96a549530ba41

---
+++ freezer_api/tests/unit/v2/test_clients.py
@@ -0,0 +1,116 @@
+# (c) Copyright 2018 ZTE Corporation.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import falcon
+import mock
+
+from freezer_api.api.v2 import clients as v2_clients
+from freezer_api.common import exceptions
+from freezer_api.tests.unit import common
+
+
+class TestClientsCollectionResource(common.FreezerBaseTestCase):
+    def setUp(self):
+        super(TestClientsCollectionResource, self).setUp()
+        self.mock_db = mock.Mock()
+        self.mock_req = mock.MagicMock()
+        self.mock_req.env.__getitem__.side_effect = common.get_req_items
+        self.mock_req.get_header.return_value = common.fake_data_0_user_id
+        self.mock_req.status = falcon.HTTP_200
+        self.resource = v2_clients.ClientsCollectionResource(self.mock_db)
+        self.mock_json_body = mock.Mock()
+        self.mock_json_body.return_value = {}
+        self.resource.json_body = self.mock_json_body
+
+    def test_on_get_return_empty_list(self):
+        self.mock_db.get_client.return_value = []
+        expected_result = {'clients': []}
+        self.resource.on_get(self.mock_req, self.mock_req,
+                             common.fake_client_info_0['project_id'])
+        result = self.mock_req.body
+        self.assertEqual(expected_result, result)
+        self.assertEqual(falcon.HTTP_200, self.mock_req.status)
+
+    def test_on_get_return_correct_list(self):
+        self.mock_db.get_client.return_value = [common.fake_client_entry_0,
+                                                common.fake_client_entry_1]
+        expected_result = {'clients': [common.fake_client_entry_0,
+                                       common.fake_client_entry_1]}
+        self.resource.on_get(self.mock_req, self.mock_req,
+                             common.fake_client_info_0['project_id'])
+        result = self.mock_req.body
+        self.assertEqual(expected_result, result)
+        self.assertEqual(falcon.HTTP_200, self.mock_req.status)
+
+    def test_on_post_raises_when_missing_body(self):
+        self.mock_db.add_client.return_value = common.fake_client_info_0[
+            'client_id']
+        self.assertRaises(exceptions.BadDataFormat, self.resource.on_post,
+                          self.mock_req, self.mock_req,
+                          common.fake_client_info_0['project_id'])
+
+    def test_on_post_inserts_correct_data(self):
+        self.mock_json_body.return_value = common.fake_client_info_0
+        self.mock_db.add_client.return_value = common.fake_client_info_0[
+            'client_id']
+        expected_result = {'client_id': common.fake_client_info_0['client_id']}
+        self.resource.on_post(self.mock_req, self.mock_req,
+                              common.fake_client_info_0['project_id'])
+        self.assertEqual(falcon.HTTP_201, self.mock_req.status)
+        self.assertEqual(expected_result, self.mock_req.body)
+
+
+class TestClientsResource(common.FreezerBaseTestCase):
+    def setUp(self):
+        super(TestClientsResource, self).setUp()
+        self.mock_db = mock.Mock()
+        self.mock_req = mock.MagicMock()
+        self.mock_req.env.__getitem__.side_effect = common.get_req_items
+        self.mock_req.get_header.return_value = common.fake_data_0_user_id
+        self.mock_req.status = falcon.HTTP_200
+        self.resource = v2_clients.ClientsResource(self.mock_db)
+        self.mock_json_body = mock.Mock()
+        self.mock_json_body.return_value = {}
+        self.resource.json_body = self.mock_json_body
+
+    def test_create_resource(self):
+        self.assertIsInstance(self.resource, v2_clients.ClientsResource)
+
+    def test_on_get_return_no_result_and_404_when_not_found(self):
+        self.mock_db.get_client.return_value = []
+        self.mock_req.body = None
+        self.resource.on_get(self.mock_req, self.mock_req,
+                             common.fake_client_info_0['project_id'],
+                             common.fake_client_info_0['client_id'])
+        self.assertIsNone(self.mock_req.body)
+        self.assertEqual(falcon.HTTP_404, self.mock_req.status)
+
+    def test_on_get_return_correct_data(self):
+        self.mock_db.get_client.return_value = [common.fake_client_entry_0]
+        expected_result = common.fake_client_entry_0
+        self.resource.on_get(self.mock_req, self.mock_req,
+                             common.fake_client_info_0['project_id'],
+                             common.fake_client_info_0['client_id'])
+        result = self.mock_req.body
+        self.assertEqual(expected_result, result)
+        self.assertEqual(falcon.HTTP_200, self.mock_req.status)
+
+    def test_on_delete_removes_proper_data(self):
+        self.resource.on_delete(self.mock_req, self.mock_req,
+                                common.fake_client_info_0['project_id'],
+                                common.fake_client_info_0['client_id'])
+        result = self.mock_req.body
+        expected_result = {'client_id': common.fake_client_info_0['client_id']}
+        self.assertEqual(falcon.HTTP_204, self.mock_req.status)
+        self.assertEqual(expected_result, result)
