1937caa1443129dd757698e2c6df00a4cebb1292
gengchc2 <geng.changcai2@zte.com.cn>
2018-10-23 18:07:18 -0700

    Add sqlalchemy migration in v2 for bp add-mysql-support-for-freezer
    Add sqlalchemy migration frame in v2.
    The test will be added in another patch.
    
    ref: https://storyboard.openstack.org/#!/story/2004132
    
    Story: #2004132
    Task: #27578
    
    Change-Id: Iec8f96a68f9e640b96c578af09fb5e4410536592
    Implements: bp add-mysql-support-for-freezer

---
+++ freezer_api/db/sqlalchemy/migrate_repo/versions/001_freezer_api_init.py
@@ -0,0 +1,32 @@
+#   Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+
+from sqlalchemy import MetaData
+
+CLASS_NAME = 'default'
+
+
+def define_tables(meta):
+
+    return []
+
+
+def upgrade(migrate_engine):
+    meta = MetaData()
+    meta.bind = migrate_engine
+
+    # create all tables
+    # Take care on create order for those with FK dependencies
+    tables = define_tables(meta)
+
+    for table in tables:
+        table.create()
