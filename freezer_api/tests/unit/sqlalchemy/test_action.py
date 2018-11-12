421cb41f4c835ba97b54b9111ff82099e73d97f3
gecong1973 <ge.cong@zte.com.cn>
2018-11-12 00:01:17 -0800

    Add Action unit test for sql
    The patch add action unit add and get test for sqlalchemy
    
    Change-Id: I1444fe89bf8d91f862d18112bb1f32a961887d39

--- freezer_api/tests/unit/sqlalchemy/test_action.py
+++ freezer_api/tests/unit/sqlalchemy/test_action.py
@@ -15,9 +15,53 @@
 
 """Tests for manipulating Action via the DB API"""
 
+import copy
 
+from freezer_api.tests.unit import common
 from freezer_api.tests.unit.sqlalchemy import base
 
 
 class DbActionTestCase(base.DbTestCase):
-    pass
+
+    def setUp(self):
+        super(DbActionTestCase, self).setUp()
+        self.fake_action_0 = common.get_fake_action_0()
+        self.freezer_action_0 = self.fake_action_0.get('freezer_action')
+
+    def test_add_and_get_action(self):
+        action_doc = copy.deepcopy(self.fake_action_0)
+        action_id = self.dbapi.add_action(user_id=self.fake_action_0.
+                                          get('user_id'),
+                                          doc=action_doc,
+                                          project_id="myproject")
+        self.assertIsNotNone(action_id)
+
+        result = self.dbapi.get_action(project_id="myproject",
+                                       user_id=self.fake_action_0.
+                                       get('user_id'),
+                                       action_id=action_id)
+
+        self.assertIsNotNone(result)
+
+        self.assertEqual(result.get('max_retries'),
+                         self.fake_action_0.get('max_retries'))
+
+        self.assertEqual(result.get('max_retries_interval'),
+                         self.fake_action_0.get('max_retries_interval'))
+
+        freezer_action = result.get('freezer_action')
+
+        self.assertEqual(freezer_action.get('action'),
+                         self.freezer_action_0.get('action'))
+
+        self.assertEqual(freezer_action.get('backup_name'),
+                         self.freezer_action_0.get('backup_name'))
+
+        self.assertEqual(freezer_action.get('container'),
+                         self.freezer_action_0.get('container'))
+
+        self.assertEqual(freezer_action.get('src_file'),
+                         self.freezer_action_0.get('src_file'))
+
+        self.assertEqual(freezer_action.get('mode'),
+                         self.freezer_action_0.get('mode'))
