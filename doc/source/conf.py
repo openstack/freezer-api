cdd34ec059a0ef942a2aaa0e93bc9dd2cb682a46
gengchc2 <geng.changcai2@zte.com.cn>
2018-11-28 22:36:16 -0800

    Add search option for list query
    Change-Id: I3a2f021f5d9e104539fc1550713553f6c86df553

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -141,12 +141,12 @@ html_theme = 'default'
 #html_last_updated_fmt = '%b %d, %Y'
 git_cmd = ["git", "log", "--pretty=format:%ad, commit %h", "--date=local",
            "-n1"]
-try:
-    html_last_updated_fmt = subprocess.check_output(git_cmd).decode('utf-8')
-except Exception:
-    warnings.warn('Cannot get last updated time from git repository. '
-                  'Not setting "html_last_updated_fmt".')
-
+#try:
+#    html_last_updated_fmt = subprocess.check_output(git_cmd).decode('utf-8')
+#except Exception:
+#    warnings.warn('Cannot get last updated time from git repository. '
+#                  'Not setting "html_last_updated_fmt".')
+html_last_updated_fmt = '%Y-%m-%d %H:%M'
 # If true, SmartyPants will be used to convert quotes and dashes to
 # typographically correct entities.
 #html_use_smartypants = True
