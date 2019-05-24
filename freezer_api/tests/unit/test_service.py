f94229aeada5ac91bfbccca4e1485bdb97877084
Cai Hui <cai.hui@zte.com.cn>
2019-05-24 05:02:22 -0400

    Remove  code that supports  older version falcon
    In the requierment.txt, falcon version is required to be greater than 1.0 ("falcon>=1.0"),
    So remove code that supports older version falcon.
    
    Change-Id: I803397a6b0f0269d1548718aa8826f0a73521405

--- freezer_api/tests/unit/test_service.py
+++ freezer_api/tests/unit/test_service.py
@@ -33,38 +33,6 @@ class TestService(common.FreezerBaseTestCase):
     @patch('freezer_api.cmd.api.v1')
     @patch('freezer_api.cmd.api.manager')
     @patch('freezer_api.cmd.api.falcon')
-    def test_on_old_falcon_builds_v0(self, mock_falcon, mock_driver, mock_v1):
-        """Test that falcon versions that should use old middleware syntax do so
-
-        :param mock_falcon: The falcon import freezer-api will try to
-        start up
-        :param mock_driver: Database driver
-        :param mock_v1: List of endpoints for v1 for the freezer API.
-        """
-        mock_driver.get_db.return_value = None
-        mock_v1.endpoints = []
-
-        # Iterate through all of the versions of falcon that should be using
-        # the old before=,after= invocation and ensure that freezer-api isn't
-        # trying to invoke it in the old style.
-        for version_string in self.falcon_versions_hooks:
-            version_attribute = '__version__' if hasattr(
-                falcon, '__version__') else 'version'
-            with patch('falcon.' + version_attribute, version_string):
-                # Attempt to invoke a mocked version of falcon to see what args
-                # it was called with
-                service.freezer_appv1_factory(None)
-
-                # Check kwargs to see if the correct arguments are being passed
-                _, named_args = mock_falcon.API.call_args
-
-                self.assertIn('before', named_args)
-                self.assertIn('after', named_args)
-                self.assertNotIn('middleware', named_args)
-
-    @patch('freezer_api.cmd.api.v1')
-    @patch('freezer_api.cmd.api.manager')
-    @patch('freezer_api.cmd.api.falcon')
     def test_on_new_falcon_builds_v1(self, mock_falcon, mock_driver, mock_v1):
         """Test that falcon versions that should use new middleware syntax do so
 
